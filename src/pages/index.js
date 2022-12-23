d152c0b54c3d1a98612b339206d6eebb7903938c
Garm <melsom.henrik@gmail.com>
2022-12-23 17:17:07 +0100

    clean slate

--- src/pages/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export default () => (
-    <div style={{margin: "3rem auto", maxWidth: 400}}>
-        <h1>Hello world!</h1>
-        <h2>Welcome to Arctic Beard Studio!</h2>
-        <p>Our site is currently under construction please head over to our <a href="https://www.facebook.com/ArcticBeardStudio">Facebook</a> page in the meanwhile :)</p>
-    </div>
-)
