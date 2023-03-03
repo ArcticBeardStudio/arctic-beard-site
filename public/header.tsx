3ac0b150256a008cef85863e637e598d0ec7829f
Garm <melsom.henrik@gmail.com>
2023-03-03 11:40:41 +0100

    added renovation notice and public path

--- public/header.tsx
+++ public/header.tsx
@@ -2,6 +2,7 @@ export default function Header() {
   return (
     <header style={{ backgroundImage: `url("/images/header-img.jpg")` }}>
       <img src={"/images/ABS_Text.svg"} alt="ABS Logo" />
+      <p>🚧 WE'RE RENOVATING 🚧</p>
       <div class={"dimmer"} />
     </header>
   );
